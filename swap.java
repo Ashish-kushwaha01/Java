1f601cc9a3cfa3ac86119f5423621377f8e66e10
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-22 18:45:07 +0530

    Swap using third variable

---
+++ swap.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        int a ,b ,c;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your first no. : ");
+        a=s.nextInt();
+        System.out.println("Enter your second no. : ");
+        b=s.nextInt();
+
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println( "After swapping : ");
+        System.out.println("My first no is : "+a);
+        System.out.println("My second no is : "+b);
+
+
+    }
+    
+}
