42e57227bfd1f610083b6c962a00874361af0f05
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-25 00:17:25 +0530

    Finding Roots of a Quadratic Equation

---
+++ Roots.java
@@ -0,0 +1,22 @@
+/* Java program to find roots of quadratic equation in all cases . */
+
+import java.util.Scanner;
+import java.math.*;
+
+public class Roots {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter coefficient of x^2 : ");
+        a=sc.nextInt();
+
+        System.out.println("Enter coefficient of x : ");
+        b=sc.nextInt();
+
+        System.out.println("Enter constant of my quadratic equation : ");
+        c=sc.nextInt();
+
+        int d = (b*b)-4*a*c;
+        int s = sqrt(d)
+    }
+}
