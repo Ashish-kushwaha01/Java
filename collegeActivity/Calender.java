09bf500eed9a0d1ee94acbef0c237d87ab70a028
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-26 18:16:11 +0530

    Calender Making

---
+++ collegeActivity/Calender.java
@@ -0,0 +1,290 @@
+package collegeActivity;
+
+public class Calender {
+    public static void main(String[] args) {
+        int month[] = new int [12];
+        
+        month[0]=31;
+       month[1]=28;
+       month[2]=31;
+       month[3]=30;
+       month[4]=31;
+       month[5]=30;
+       month[6]=31;
+       month[7]=31;
+       month[8]=30;
+       month[9]=31;
+       month[10]=30;
+       month[11]=31;
+
+      for(int i=0;i<12;i++){
+        
+       if(i==0){
+        System.out.println("January has "+month[i]+ " days.");
+        int days[][]=new int[5][7];
+        for(int j=0;j<5;j++){
+            for(int k= (7*j)+1;k<=7*(j+1);k++){
+               
+        
+      
+                   if((k)<=month[i]){
+                    System.out.print(k + "\s");
+                    continue;
+                   }
+                   else{
+                    System.out.print("");
+                   }
+                
+                }
+                System.out.print("\n");
+        
+            }
+        
+       }
+        else if  (i==1){
+            System.out.println("February has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==2){
+            System.out.println("March has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==3){
+            System.out.println("April has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==4){
+            System.out.println("May has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==5){
+            System.out.println("June has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==6){
+            System.out.println("July has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==7){
+            System.out.println("August has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==8){
+            System.out.println("September has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==9){
+            System.out.println("October has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==10){
+            System.out.println("Novenber has "+month[i]+ " days.");
+            System.out.println("January has "+month[i]+ " days.");
+            int days[][]=new int[5][7];
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+            
+        } else if  (i==11){
+            System.out.println("December  has "+month[i]+ " days.");
+            for(int j=0;j<5;j++){
+                for(int k= (7*j)+1;k<=7*(j+1);k++){
+                   
+            
+          
+                       if((k)<=month[i]){
+                        System.out.print(k + "\s");
+                        continue;
+                       }
+                       else{
+                        System.out.print("");
+                       }
+                    
+                    }
+                    System.out.print("\n");
+            
+                }
+        
+        }
+        
+      }
+    }
+    
+}
+
