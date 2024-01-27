927a4b5193b07a69b60d8f334c1a7eb3cf05104d
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-27 15:50:50 +0530

    Factorial Of a given number

---
+++ collegeActivity/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your no. : ");
+        n=sc.nextInt();
+        
+        int fact=1;
+        if(n==0){
+            System.out.print( "Factorial is : " +1 );
+        }
+        else{
+        for(int i=n;i>=1;i--){
+            fact*=i;
+        }
+         System.out.println("Factorial is : "+fact);
+        
+    }
+            
+       
+    }
+}
