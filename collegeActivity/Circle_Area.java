8aed25e0ea680e3b672cd4d9c663a44136800527
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-27 17:19:06 +0530

    Area of a Circle

---
+++ collegeActivity/Circle_Area.java
@@ -0,0 +1,16 @@
+package collegeActivity;
+
+import java.util.Scanner;
+
+public class Circle_Area {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int r;
+    System.out.println("Enter radius of a circle  : ");
+    r=sc.nextInt();
+// double pi=3.14;
+    double Area = (3.14)*r*r;
+
+    System.out.println("Area of a given circle is : "+Area);
+   } 
+}
