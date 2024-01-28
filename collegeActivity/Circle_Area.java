ac4f0187f6c201a201c645dc44bf8889093d4564
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-28 18:51:24 +0530

    College Questions

--- collegeActivity/Circle_Area.java
+++ collegeActivity/Circle_Area.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Circle_Area {
    public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    int r;
+    double r;
     System.out.println("Enter radius of a circle  : ");
     r=sc.nextInt();
 // double pi=3.14;
