d7462cef2790c15aabc26343335dddf945d0f461
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-28 21:55:21 +0530

    Use of Typecasting

---
+++ collegeActivity/TypeCasting.java
@@ -0,0 +1,12 @@
+package collegeActivity;
+
+public class TypeCasting {
+
+   public static void main(String[] args) {
+    for(int i=65;i<=122;i++){
+        System.out.print(i+" is the ASCII VALUE of "+(char)i);
+        System.out.println("");
+    }
+   }
+    
+}
