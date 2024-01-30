0ab22d320cc81b9f20bb195bc4521f5763b334a9
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-01-30 23:48:59 +0530

    Add Rheumer in Temperature

--- collegeActivity/Temperature.java
+++ collegeActivity/Temperature.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in); 
-            float celcius,farn,kelvin;
+            float celcius,farn,kelvin,Rheumer;
             System.out.println("Enter your temperature in Celcius : ");
             celcius=sc.nextFloat();
 
             farn =( 9*celcius/5)+32;
             kelvin= celcius+273;
-
-            System.out.println("Your temperature in fahranheit is : " +farn+" and in kelvin is : "+kelvin );
+            Rheumer=4*celcius/5;
+            System.out.println("Your temperature in fahranheit is : " +farn+" and in kelvin is : "+kelvin +"and in Rheumer is : "+Rheumer);
         
         }
 }
