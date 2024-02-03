7f1ecc8112f59ad9b3bf7bd43882c3c0f5bbd585
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-03 22:19:50 +0530

    Reverse a String without using inbuilt function

---
+++ String/Reverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Reverse{
+    public static void main(String[] args) {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your string : ");
+        str= sc.nextLine();
+
+        int len = str.length();
+        String rev="";
+        for(int i=len-1;i>=0;i--){
+        
+        rev=rev+ str.charAt(i); 
+        
+    }
+    System.out.print("Reverse of my string is : " +rev);
+}
+}
