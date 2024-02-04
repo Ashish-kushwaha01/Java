3e6f4965c6c87c1590aa97530fee6074c47a3f41
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-04 23:59:29 +0530

    Swap two string without using third variable

---
+++ String/Swap_String.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Scanner;
+
+// Swap two string without using third variable;
+public class Swap_String {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1,s2;
+        System.out.println("Enter your first string : ");
+        s1=sc.nextLine();
+        int len=s1.length();
+        
+        System.out.println("Enter your second string : ");
+        s2=sc.nextLine();
+
+            s1= s1.concat(s2);
+            s2=s1.substring(0, len);
+            s1=s1.substring(len, s1.length());
+
+            System.out.println("First string is : "+s1);
+            System.out.println("Second string is : "+s2);
+
+    }
+}
