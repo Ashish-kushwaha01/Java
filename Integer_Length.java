6cde6c7ba4f6eb2e21a07b286c8cd7106349e869
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-04 16:03:26 +0530

    Reverse of my no.

--- Integer_Length.java
+++ Integer_Length.java
@@ -8,10 +8,19 @@ public class Integer_Length{
         System.out.println("Enter your no. : ");
         x=sc.nextInt();
         int y=0;
+        int rev=0;
+        int n=x;
         while(x!=0){
+            int digit = x%10;
+            rev = (rev*10)+digit;
             x=x/10;
+            
             y++;
         }
         System.out.println("Total digits in my element is : "+y);
+        if(y>= 10)
+        System.out.println(0);
+        else
+        System.out.println("Reverse of my integer is : "+rev);
     }
 }
