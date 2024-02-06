3d8d4548dde5df568ac6cf3c4ea38a25879aefed
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-06 19:19:13 +0530

    Occurence of a given character

--- String/Occurance.java
+++ String/Occurance.java
@@ -6,17 +6,26 @@ import java.util.Scanner;
 public class Occurance {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String str = "Ashish Kushwaha";
+        String str ;
+        System.out.println("Enter your string : ");
+        str= sc.nextLine();
         int strlen = str.length();
         System.out.println("Length of my string is  : "+strlen);
         
+        int count =0;
         char ch;
-        // System.out.println("Which charactr do you want to search ? : ");
-        // ch=sc.next().charAt(0);
-
-        int Now_Strlen= str.replace("s4", "").length();
-        int count = strlen - Now_Strlen;
-
-        System.out.println("Total no. of given character in my string is : "+count);
+        System.out.println("Which charactr do you want to search ? : ");
+        ch=sc.next().charAt(0);
+       
+       for(int j=0;j<str.length();j++){
+         for(int i=65;i<=122;i++){
+            if((int)ch==i){
+                count++;
+            }
+            }
+              continue;
+        }
+        System.out.println("Your given characte ("+ch+") is : "+count+" times");
+       
     }
 }
