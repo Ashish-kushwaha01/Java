0fe81249b1edac5a43bd3efcf7069366d8db1a64
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-10 19:05:10 +0530

    Making class in same Package

--- HomeActivity/Student.java
+++ HomeActivity/Student.java
@@ -1,36 +1,37 @@
 package HomeActivity;
 
  public class Student{
-    int roll=23;
-    String name;
-    double marks ;
-    double percentage ;
+   public int roll =45;
+   public String name="Ashish";
+    double marks = 765.8 ;
+    double percentage= 98 ;
+ 
 
     // Making Constructor .
 
 
-    // public Student(String naam,int rollNo,double ank){
-    //     name = naam;
-    //     roll=rollNo;
-    //     marks=ank;
-    // }
+    public Student(String naam,int rollNo,double ank){
+        name = naam;
+        roll=rollNo;
+        marks=ank;
+    }
     public Student(){
         
     }
     
-    public Student(String name,int roll,double marks){
-        this.roll=roll;
-        this.marks=marks;
-        this.name =name;
-    }
+    // public Student(String name,int roll,double marks){
+    //     this.roll=roll;
+    //     this.marks=marks;
+    //     this.name =name;
+    // }
 
     // using of getter and setter .
-    public int getRoll(){
-        return roll;
+    // public int getRoll(){
+    //     return roll;
+    // }
+
+    // public void setRoll(int n){
+    //     roll = n;
     }
 
-    public void setRoll(int n){
-        roll = n;
-    }
-
-}
+// }
