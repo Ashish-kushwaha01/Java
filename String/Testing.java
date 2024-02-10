e8a1af96affd57d004550d55d973a4fbb9a99bc9
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-10 19:06:39 +0530

    Class testing in other package

--- String/Testing.java
+++ String/Testing.java
@@ -1,10 +1,10 @@
 package String;
 
-import HomeActivity.Student;
+import HomeActivity.*;
 
 public class Testing {
     public static void main(String[] args) {
         Student s=new Student();
-       
+       System.out.println(s.name);
     }
 }
