bb6354f16d63c03939f37285e7338095855d0df3
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-12 22:37:37 +0530

    HCF of two no.

--- Large.java
+++ Large.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 class Large{
 
     
-
+//   First Method .
 
 public static int HCF(int a, int b){
     if(a!=b){
@@ -18,6 +18,19 @@ public static int HCF(int a, int b){
     }
     return a;
 }
+
+public static int GCD(int a,int b){
+    if(a<b && b%a==0)
+    return a;
+    if(b<a && a%b==0) return b;
+    while (b>0){
+        int temp=b;
+        b=b%a;
+        a=temp;
+    }
+    return b;
+}
+
 public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int a, b;
@@ -26,6 +39,8 @@ public static void main(String[] args) {
     b=sc.nextInt();
 
     System.out.println("HCF of "+a+" and "+b+" is : "+HCF(a, b));
+    
+    System.out.println("HCF of "+a+" and "+b+" is : "+GCD(a, b));
     
 }
 
