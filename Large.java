f661e3dbb7e64c95d01e76ce9e7069794a8080b4
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-13 21:40:38 +0530

    HCF of two no.

--- Large.java
+++ Large.java
@@ -23,18 +23,30 @@ public static int HCF(int a, int b){
 // SECOND METHOD . 
 
 
-public static int GCD(int a,int b){
-    if(a<b && b%a==0)
-    return a;
-    if(b<a && a%b==0) return b;
-    while (b>0){
-        int temp=b;
-        b=b%a;
-        a=temp;
-    }
-    return b;
-}
+// public static int GCD(int a,int b){
+//     if(a<b && b%a==0)
+//     return a;
+//     if(b<a && a%b==0) return b;
+//     while (b>0){
+//         int temp=b;
+//         b=b%a;
+//         a=temp;
+//     }
+//     return b;
+// }
 
+
+// NEXT METHOD .
+
+public static int GCD(int a, int b){
+    int min=Math.min(a, b);
+   for(int i=min;i>=1;i++){
+    if(a%i==0 && b%i==0)
+    min=i;
+   }
+   return min;
+    
+}
 public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int a, b;
