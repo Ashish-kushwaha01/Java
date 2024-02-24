f0a8d7ac94517b109b17a66bf7c5145447763225
Ashish Kushwaha <ashishkushwaha872611@gmail.com>
2024-02-24 21:35:00 +0530

    Change lower case into upper case and upper case into lower case

---
+++ String/Lower_Case.java
@@ -0,0 +1,12 @@
+package String;
+
+public class Lower_Case {
+    public static void main(String[] args) {
+        String s="ASHISH";
+        String p=s.toLowerCase();
+        System.out.println(p);
+        String c = "Ashish";
+        String d=c.toUpperCase();
+        System.out.println(d);
+    }
+}
